b0135f454917a9fae37d720fc8f7122b647d7398
SubhashRowtu <78291367+subhash546@users.noreply.github.com>
2021-05-16 20:18:58 +0530

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<cs50.h>
+int main(void)// main method
+
+{
+    //to a name using get string function
+
+    string name=get_string("what is your name?\n");
+    //to print hello before the string name
+    printf("hello world,%s\n",name);
+}
+© 2021 GitHub, Inc.
